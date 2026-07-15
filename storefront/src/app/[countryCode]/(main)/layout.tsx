b62b79bfc30cdd163cb578cc09b07e906e34a1e5
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-15 17:31:17 +0200

    Update layout.tsx

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -4,8 +4,10 @@ import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
 import { getBaseURL } from "@lib/util/env"
 
-export const metadata: Metadata = {
-  metadataBase: new URL(getBaseURL()),
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    metadataBase: new URL(await getBaseURL()),
+  }
 }
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
