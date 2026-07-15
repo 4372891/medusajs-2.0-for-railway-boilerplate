6f4e85c91879cb74f262e2fafd07eb371c810d26
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-15 17:07:56 +0200

    Update layout.tsx

--- storefront/src/app/[countryCode]/(checkout)/layout.tsx
+++ storefront/src/app/[countryCode]/(checkout)/layout.tsx
@@ -1,12 +1,14 @@
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import ChevronDown from "@modules/common/icons/chevron-down"
 import MedusaCTA from "@modules/layout/components/medusa-cta"
+import { getStoreName } from "@lib/tenants"
 
-export default function CheckoutLayout({
+export default async function CheckoutLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const storeName = await getStoreName()
   return (
     <div className="w-full bg-white relative small:min-h-screen">
       <div className="h-16 bg-white border-b ">
@@ -29,7 +31,7 @@ export default function CheckoutLayout({
             className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
             data-testid="store-link"
           >
-            Medusa Store
+            {storeName}
           </LocalizedClientLink>
           <div className="flex-1 basis-0" />
         </nav>
