59c3aea808e3d7c482097fc611dcd429b2e261f3
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-15 16:08:35 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next"
 
 import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
 import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
@@ -26,7 +25,6 @@ export default async function Home({
 
   return (
     <>
-      <Hero />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
