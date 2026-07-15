c8256832745efbd5ce376266342795b72bb68c2a
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-15 19:51:40 +0200

    Update middleware.ts

--- storefront/src/middleware.ts
+++ storefront/src/middleware.ts
@@ -18,6 +18,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.jpg|.*\\.gif|.*\\.svg).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.png|.*\\.jpg|.*\\.gif|.*\\.svg).*)",
   ],
 }
