efd9bd03111e6e9c2989774442769b76493305ef
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-15 19:08:29 +0200

    Update cart.ts

--- storefront/src/lib/data/cart.ts
+++ storefront/src/lib/data/cart.ts
@@ -345,7 +345,7 @@ export async function setAddresses(currentState: unknown, formData: FormData) {
   }
 
   redirect(
-    `/${formData.get("shipping_address.country_code")}/checkout?step=delivery`
+    redirect(`/checkout?step=delivery`)
   )
 }
 
