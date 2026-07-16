e1b42c8567c9b8d2cb2653e5b5379e771188329c
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-16 12:27:04 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/store/page.tsx
+++ storefront/src/app/[countryCode]/(main)/store/page.tsx
@@ -1,31 +1,5 @@
-import { Metadata } from "next"
+import { redirect } from "next/navigation"
 
-import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-import StoreTemplate from "@modules/store/templates"
-
-export const metadata: Metadata = {
-  title: "Store",
-  description: "Explore all of our products.",
-}
-
-type Params = {
-  searchParams: {
-    sortBy?: SortOptions
-    page?: string
-  }
-  params: {
-    countryCode: string
-  }
-}
-
-export default async function StorePage({ searchParams, params }: Params) {
-  const { sortBy, page } = searchParams
-
-  return (
-    <StoreTemplate
-      sortBy={sortBy}
-      page={page}
-      countryCode={params.countryCode}
-    />
-  )
+export default function StorePage() {
+  redirect("/")
 }
