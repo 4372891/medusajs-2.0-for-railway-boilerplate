2f0d69b37fd656de090a43ad7bd4ca1583f0c77b
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-25 18:32:29 +0200

    Update index.tsx

--- storefront/src/modules/products/components/product-preview/index.tsx
+++ storefront/src/modules/products/components/product-preview/index.tsx
@@ -38,8 +38,12 @@ export default async function ProductPreview({
           size="full"
           isFeatured={isFeatured}
         />
-        <div className="flex txt-compact-medium mt-4 justify-between">
-          <Text className="text-ui-fg-subtle" data-testid="product-title">
+        <div className="flex flex-col txt-compact-medium mt-4">
+          <Text
+            className="text-ui-fg-subtle truncate w-full"
+            title={product.title}
+            data-testid="product-title"
+          >
             {product.title}
           </Text>
           <div className="flex items-center gap-x-2">
