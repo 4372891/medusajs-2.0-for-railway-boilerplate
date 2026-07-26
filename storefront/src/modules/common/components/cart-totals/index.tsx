0bebd2047c563ae2e9220a44e3d8c60d5adf0437
4372891 <129066689+4372891@users.noreply.github.com>
2026-07-26 18:10:45 +0200

    Update index.tsx

--- storefront/src/modules/common/components/cart-totals/index.tsx
+++ storefront/src/modules/common/components/cart-totals/index.tsx
@@ -4,7 +4,6 @@ import { convertToLocale } from "@lib/util/money"
 import { InformationCircleSolid } from "@medusajs/icons"
 import { Tooltip } from "@medusajs/ui"
 import React from "react"
-import CurrencyNote from "@modules/common/components/currency-note"
 
 type CartTotalsProps = {
   totals: {
